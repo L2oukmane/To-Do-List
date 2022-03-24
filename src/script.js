cf266e0b93ee879fe1431bccc7e74f6026e3ee00
Lucas Shangrita <oustanilokman@gmail.com>
2022-03-24 17:14:15 +0300

    Add methods for add and remove

--- src/script.js
+++ src/script.js
@@ -1,34 +1,11 @@
 import './style.css';
+import List from './AddRemove.js';
 
-const todoList = document.querySelector('.items');
-const listForm = document.querySelector('.list-form');
-const input = document.querySelector('.input');
-class ToDo {
-    constructor(description, completed, index){
-        this.description = description;
-        this.completed = completed;
-        this.index = index;
-    }
-	}
+const tasks = new List();
 
-class ToDoDataBase {
-	constructor(){
-		this.ToDo = JSON.parse(localStorage.getItem('bookStorage')) || [];
-	}
-    addNote(){
-    listForm.addEventListener('submit', () => {
-    const currentNote = new ToDo(input.value);
-		this.ToDo.push(currentNote);
-		input = '';
-    });
-		localStorage.setItem('bookStorage', JSON.stringify(this.ToDo));
-    this.localstorage();
-    }
-
-		showNote(description, completed, index){
-			if(description == ''){
-				listForm.removeEventListener('submit', false, false);
-			}
-
-		}
-	}
+document.querySelector('.list-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const activity = e.target.elements.activity.value;
+  tasks.addActivity(activity);
+  e.target.reset();
+});
